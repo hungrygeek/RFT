e2a3763135de4c07a05ed957bedfb5307f56515c
Charles He <heqiurui@gmail.com>
2014-04-23 16:11:37 +0100

    Modified refresh frequency of line charts. Also minor bug fix on the initial display order of line charts.

--- www/rft.js
+++ www/rft.js
@@ -1,6 +1,6 @@
 var dataUrl = 'sample_data.json';
 var dataUpdateMs = 60000; // 1 minute
-var lineUpdateMs = 10000; // 10 seconds
+var lineUpdateMs = 5000; // 10 seconds
 
 var rftData = fetchData(dataUrl);
 var barChart, lineChart1, lineChart2;
@@ -9,9 +9,9 @@ AmCharts.ready(function () {
   barChart   = new RFTChart('barchart',  'barchart',   'bartitle',
                             rftData.rankingChart);
   lineChart1 = new RFTChart('linechart', 'linechart1', 'linetitle1',
-                            rftData.historyCharts, 0);
+                            rftData.historyCharts, rftData.numRooms - 1);
   lineChart2 = new RFTChart('linechart', 'linechart2', 'linetitle2',
-                            rftData.historyCharts, 1);
+                            rftData.historyCharts, 0);
 });
 
 function fetchData (url) {
